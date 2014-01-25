3ce3fea2b18a99fb2697ed940377f9ef4b9d5b78
Sebastien Andary <sebastien@naturalpad.org>
2014-01-25 18:59:37 +0100

    update

---
+++ Assets/Limp.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Limp : MonoBehaviour {
+    public AnimationClip clip;
+    public float movementThreshold = 1;
+    CharacterController controller;
+	// Use this for initialization
+	void Start () {
+        clip.wrapMode = WrapMode.Once;
+        animation.clip = clip;
+        controller = GameObject.FindObjectOfType<CharacterController>();
+	}
+
+    float movementAmount;
+
+    // Update is called once per frame
+	void Update () {
+        movementAmount += controller.velocity.magnitude * Time.deltaTime;
+        if (movementAmount >= movementThreshold)
+        {
+            if (!animation.isPlaying)
+                animation.Play();
+            movementAmount -= movementThreshold;
+        }
+    }
+}
