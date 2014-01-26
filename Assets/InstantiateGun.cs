9f7563ac1b7468a23bc7bfb0abf4d2c79c5d8b51
Sebastien Andary <sebastien@naturalpad.org>
2014-01-26 10:15:13 +0100

    update level

--- Assets/InstantiateGun.cs
+++ Assets/InstantiateGun.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class InstantiateGun : MonoBehaviour {
     public GameObject gunPrefab;
+    public Vector3 gunPosition;
+    public float time = 0.8f;
+
     void pickUp(GameObject obj)
     {
         if (obj.name == "gun")
         {
             gunPrefab.SetActive(true);
             Destroy(obj);
+            iTween.MoveTo(gunPrefab, iTween.Hash(
+                "time", time,
+                "position", gunPosition,
+                "islocal", true,
+                "easetype", iTween.EaseType.easeOutQuad
+                ));
         }
     }
 }
